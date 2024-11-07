74332dbe55a0505a5fb92119f44aefe46bca57a3
keito654 <keito19981129@gmail.com>
2024-11-07 16:14:03 +0900

    fix

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import asyncio
-from operation.operate import query 
+from operation.operate import query, insert
 
 async def main():
+    await insert()
     await query()
 
 if __name__ == "__main__":
